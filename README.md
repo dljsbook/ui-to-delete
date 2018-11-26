2b796c25dae95168cf16ac69b7a40cb34ab54e2d
Kevin Scott <kevin@hitheory.com>
2018-11-25 21:45:53 -0500

    Update UI to use rollup instead of microbundle

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Deep Learning with Javascript supporting libraries
 
-![Travis Badge](https://travis-ci.com/dljsbook/code.svg?branch=master)
+![Travis Badge](https://travis-ci.com/dljsbook/UI.svg?branch=master)
 
 These are supporting libraries for working through the book Deep Learning with Javascript.
 
---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+// rollup.config.js
+import typescript from 'rollup-plugin-typescript';
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'esm',
+  },
+  plugins: [
+    typescript(),
+  ],
+  external: [
+    '@tensorflow/tfjs',
+    '@tensorflow/tfjs-vis',
+    'vega',
+    'papaparse',
+  ],
+}
+
